c4f5ae4685f385026a9cfd8220b9a2d7a1e52702
lim6112j <lim6112j@gmail.com>
2023-03-23 02:28:26 +0900

    recap

--- src/main/scala/part1_recap/ScalaRecap.scala
+++ src/main/scala/part1_recap/ScalaRecap.scala
@@ -25,4 +25,79 @@ object ScalaRecap extends App {
   // FP
   val anIncrementer: Int => Int = (x: Int) => x + 1
   anIncrementer(1)
+  // Pattern matching
+  val unknown: Any = 2
+  val order = unknown match {
+    case 1 => "first"
+    case 2 => "second"
+    case _ => "unknown"
+  }
+
+  //try {
+  //throw new RuntimeException
+  //} catch {
+  //case e: Exception => println("I caught one!");
+  //}
+
+  // multithreading
+  import scala.concurrent.ExecutionContext.Implicits.global
+  val future = Future {
+    // long computation here
+    // executed on some other thread
+    42
+  }
+  future.onComplete {
+    case Success(value) => println(s"I found of life: $value")
+    case Failure(ex)    => println(s"I found $ex")
+  } // on some thread
+
+  val partialFunction: PartialFunction[Int, Int] = {
+    case 1 => 42
+    case 2 => 65
+    case _ => 999
+  }
+  println(partialFunction(1))
+
+  // type aliases
+  type AkkaReceive = PartialFunction[Any, Unit]
+  def receive: AkkaReceive = {
+    case 1 => println("hello")
+    case _ => println("confused...")
+  }
+
+  // Implicits
+  implicit val timeout = 3000
+  def setTimeout(f: () => Unit)(implicit timeout: Int) = f()
+  setTimeout(() => println(timeout))
+
+  // conversions
+  // 1) implicit methods
+  case class Person(name: String) {
+    def greet: String = s"hi, my name is $name"
+  }
+  implicit def fromStringToPerson(name: String) = Person(name)
+  "peter".greet
+  // fromStringToPerson("peter").greet
+
+  // 2) implicit classes
+  implicit class Dog(name: String) {
+    def bark = println("Bark")
+  }
+  "Lassie".bark
+  // new Dog("Lassie").bark
+
+  // implicit organizations
+  // local scope
+  implicit val numberOrdering: Ordering[Int] = Ordering.fromLessThan(_ > _)
+  List(1, 2, 3).sorted // (numberOrdering) => List(3,2,1)
+
+  // imported scope
+  //
+  // companion objects of the types involved in the call
+  object Person {
+    implicit val personOrdering: Ordering[Person] =
+      Ordering.fromLessThan((a, b) => a.name.compareTo(b.name) < 0)
+  }
+  List(Person("Bob"), Person("Alice")).sorted // (Person.personOrdering)
+  // => List(Person("Alice"), Person("Bob"))
 }
